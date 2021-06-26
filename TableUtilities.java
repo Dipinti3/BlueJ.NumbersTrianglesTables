d3c0327f73f3e70c7896fa5ed7a64a555e5b5290
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-06-25 22:46:58 -0400

    made changes to the java files

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,65 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+        String result="";
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                int mulValue=i*j;
+                if(mulValue<10)
+                {
+                    result +="  "+mulValue+" |";
+                }
+                else
+                {result+=" "+mulValue+" |";}
+            }
+            result+="\n";
+        }
+        return result;
+        }
+      
+    
 
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
+        String result="";
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                int mulValue=i*j;
+                if(mulValue>99){
+                    result+=mulValue+" |";break;
+                }
+                if(mulValue<10)
+                {
+                    result +="  "+mulValue+" |";
+                }
+                else{
+                    result+=" "+mulValue+" |";
+                }
+            }
+            result+="\n";
+        }
+        return result;
+        }
+    
+
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result="";
+        for(int i=1;i<=tableSize;i++){
+            for(int j=1;j<=tableSize;j++){
+                int mulValue=i*j;
+                if(mulValue>99){
+                    result+=mulValue+" |";
+                }
+                else if(mulValue<10)
+                {
+                    result +="  "+mulValue+" |";
+                }
+                else if(mulValue>9 && mulValue<100){
+                    result+=" "+mulValue+" |";
+                }
+            }
+            result+="\n";
+        }
+        return result;
     }
 }
